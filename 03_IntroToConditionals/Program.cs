a1cebc8e62e93a1bac7a8a164006335eb7a0cd18
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-11-02 17:42:16 -0400

    completed day3

---
+++ 03_IntroToConditionals/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = 18;
+
+        int odd = N % 2;
+
+        if (odd == 1)
+        {
+            Console.WriteLine("Weird");
+        }
+        else if (odd == 0 && N<=5 && N>=2)
+        {
+            Console.WriteLine("Not Weird");
+        }
+        else if (odd == 0 && N <=20 && N >= 6)
+        {
+            Console.WriteLine("Weird");
+        }
+        else if(odd == 0 && N > 20)
+        {
+            Console.WriteLine("Not Weird");
+        }
+    }
+}

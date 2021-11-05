83bd01a7f936d5c8c2bcb3969275caea0c2e1e9a
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-11-05 10:45:27 -0400

    completed day 5

---
+++ 05_Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05_Loops
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            runTimesTable(n);
+        }
+
+        public static void runTimesTable(int value)
+        {            
+            //result should multiple value and loopcount
+            for (int loopCount = 1; loopCount < 11; loopCount++)
+            {
+                int result = value * loopCount;
+                Console.WriteLine($"{value} x {loopCount} = {result}");
+            }
+        }
+    }
+
+    
+}

e8350247d71e3dae4e2d1ec18e65b53cb8fb0828
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-11-08 17:18:58 -0500

    started outline for day6 challenge

---
+++ 06_Review/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _06_Review
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            //T represents test cases -->2
+            //read from console
+            int T = Convert.ToInt32(Console.ReadLine());
+
+            //Given string S, of length N, index from 0 --> N-1
+            string [] S = new string[] {"Hacker"};
+
+            //char[] S2 = new char[] { Convert.ToChar(Console.ReadLine()) };
+            //print the even indecies then odd, deliminate with 2 spaces
+
+            foreach (string item in S2)
+            {
+                //take initial string and add each character into new char list
+                Console.WriteLine($"{item}  ");
+            }
+        }
+    }
+}

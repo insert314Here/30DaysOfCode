440e922841b27f8816ba64086974a5bcaa018332
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-11-15 18:56:25 -0500

    started work on side project of adding value of string

---
+++ 07_Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+namespace _07_Arrays
+{
+    //Arrays hold a fixed value of a single data type
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        }
+    }
+}
---
+++ SumValueStrings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumValueStrings
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // if there is white space return result -1
+
+            // switch statement to cycle thru word hello e=5, h=8, l=12, o=15
+        }
+
+        public static int ComputeStringValue(public string word)
+        {
+            int result = 0;
+            //create a char list to store each letter in string as a single index value
+            public char[] chars = word.ToCharArray(0,word.length);
+            
+        //string [] list;
+            
+            //first condition if the string has white space, return -1 --> result =-1
+            if (string.IsNullOrWhiteSpace())
+            {
+
+            }
+
+            return result;
+        }
+    }
+}

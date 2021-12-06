e1f9188a3048c576216b5ef39523b37ae99a91b8
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-12-06 13:23:26 -0500

    finished prgram to compute value of a string using the alphabet for integer values

---
+++ SumString2/Program.cs
@@ -0,0 +1,55 @@
+﻿public class Program
+{
+  
+    public static void Main(string[] args)
+    {
+        asciiPrint();
+        /*
+        string [] str = { "sahil", "shashanak", "sanjit","abhinav", "mohit" };
+        string s = "abhinav";
+        int n = s.Length;
+        int score = stringScore(str,s,n);
+        Console.WriteLine(score);
+        */
+    }
+   
+    //function to find string score
+
+    //static int stringScore(string[] str, string s, int n)
+    //{
+    //    int score = 0, index = 0;
+    //    for (int i = 0; i < n; i++)
+    //    {
+    //        if (str[i] == s)
+    //        {
+    //            for (int j = 0; j < s.Length; j++)
+    //                score += s[j] - 'a' + 1;
+    //            index = i + 1;
+    //            break;
+    //        }            
+    //    }
+    //    score = score * index;
+    //    return score;
+    //}
+
+    //print ascii values of char
+
+    public static void asciiPrint()
+    {
+        int sum = 0;
+        int value = 0;
+        string word = Convert.ToString(Console.ReadLine());
+        word = word.ToLower();
+
+        char[] charList = word.ToCharArray();
+
+        foreach (char letter in charList)
+        {
+            //Console.WriteLine((int)letter - 96);
+            value = (int)letter - 96;
+            Console.WriteLine(value);
+        }
+    }
+}
+
+//a registers as 97 -->  97 - 96 = 1
--- SumValueStrings/Program.cs
+++ SumValueStrings/Program.cs
@@ -1,33 +1,48 @@
 ﻿using System;
+// switch statement to cycle thru word hello e=5, h=8, l=12, o=15
 
 namespace SumValueStrings
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
 
+
+        public static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Hello World!");
+            string word = Convert.ToString(Console.ReadLine());
+            
             // if there is white space return result -1
-
-            // switch statement to cycle thru word hello e=5, h=8, l=12, o=15
-        }
-
-        public static int ComputeStringValue(public string word)
-        {
-            int result = 0;
-            //create a char list to store each letter in string as a single index value
-            public char[] chars = word.ToCharArray(0,word.length);
-            
-        //string [] list;
-            
-            //first condition if the string has white space, return -1 --> result =-1
-            if (string.IsNullOrWhiteSpace())
+            bool hasSpace = word.Contains(" ");
+            if (hasSpace)
             {
-
+                Console.WriteLine("there is a space so: -1");
+            }
+            else
+            {
+                ComputeStringValue(word);
             }
 
-            return result;
+        }
+
+        public static void ComputeStringValue(string word)
+        {
+            int result = 0;
+            
+            word = word.ToLower();
+            char[] charList = word.ToCharArray(0, word.Length);
+
+            // loop through the charList to assign integer value to word
+            foreach (char letter in charList)
+            {                
+               //compensate for ASCII integer value of alphabet
+               result += (int)letter - 96;
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
+
+

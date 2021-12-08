34826cede1720fd764de7242938dcd2ce1a35d4f
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-12-08 16:22:53 -0500

    complted recursion challenge with factorial example

---
+++ 09_Recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter a number to find its factorial value");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+factorial(n);
+Console.WriteLine($"{n} Factorial is equal to: {result}");
+Console.ReadKey();
+
+
+int factorial(int n)
+{    
+
+    if (n <= 1)
+    {
+        result = 1;
+        return result;
+    }
+    else
+    {
+        result = n * factorial(n - 1);
+        return result;
+    }
+
+    
+}
